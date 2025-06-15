192939a7d3aa25d0f397b002309c8dd191e265e8
Mohamed <abdulhakimm280@gmail.com>
2025-06-15 17:52:59 +0300

    python tuples

--- app.py
+++ app.py
@@ -26,4 +26,35 @@ print(greeting.strip().upper())
 print(greeting.replace("Hello", "Yo").strip())
 print(greeting)
 
+# Lists
+fruits = ["apple", "banana", "cherry"]
+
+print(fruits[0])
+print(fruits[1])
+print("The length of the list is ", len(fruits))
+# changing the value of a list
+fruits[1] = "orange"
+print(fruits)
+# list method
+fruits.append("kiwi")
+print(fruits)
+
+fruits.remove("apple")
+print(fruits)
+
+fruits.sort()
+print(fruits)
+
+# tuples
+top_scores = (100, 90, 80)
+print(top_scores[0])
+print(top_scores[1])
+print("The length of the tuple is ", len(top_scores))
+
+# tuples are immutable
+# top_scores[0] = (20)
+
+# tuple methods
+print(top_scores.count(90))
+print(top_scores.index(80))
 
