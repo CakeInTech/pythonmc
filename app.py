698fe3b6fafb7c936026c798df9c6e5b9cd65c73
Mohamed <abdulhakimm280@gmail.com>
2025-06-15 17:01:55 +0300

    python strings and numbers

--- app.py
+++ app.py
@@ -4,10 +4,26 @@ name = "Mohamed"
 age = 21
 height =  5.9
 
+name= "abdul"
+age = 25
+height = "five ft, 2 inches"
+
 # printing variables
 print(name, age, height)
 
-
 # type errors
+print(name + str(age))
+print (10 + int("20"))
 
 # string methods
+greeting = " Hello, World! "
+
+
+print(len(greeting))
+print(greeting.strip())
+print(greeting.strip().lower())
+print(greeting.strip().upper())
+print(greeting.replace("Hello", "Yo").strip())
+print(greeting)
+
+
